ac6724017ad5ba610066eeec4901a4179b278d0c
KasuniMadhara <kasuniwijerathna2003@gmail.com>
2026-05-19 08:10:20 +0530

    Connect Routes To App

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import authRoutes from './modules/auth/auth.routes';
 
 const app = express();
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Backend Running');
