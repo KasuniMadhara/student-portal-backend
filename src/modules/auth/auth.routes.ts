f57a524914fc1a92cc208b9386fa9b4fc0ab8db4
KasuniMadhara <kasuniwijerathna2003@gmail.com>
2026-05-24 20:24:55 +0530

    Adds JWT-based authentication middleware and profile route

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,22 @@
 import express from 'express';
 import { login,register } from './auth.controller';
+import auth from '../../middlewares/auth.middleware';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+
+router.get(
+    '/profile',
+    auth,
+    (req, res) => {
+        res.status(200).json({
+            success: true,
+            message: 'User profile accessed',
+            user: req.user
+        });
+    }
+)
 
 export default router;
