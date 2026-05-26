0a631049c2eb750ce5a3c8e838b91589db6946bf
KasuniMadhara <kasuniwijerathna2003@gmail.com>
2026-05-26 23:02:25 +0530

    Adds protected route for updating user by ID

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import {getUser, getUsers, login, register } from './auth.controller';
+import {updateSingleUser,getUser, getUsers, login, register } from './auth.controller';
 import auth from '../../middlewares/auth.middleware';
 
 const router = express.Router();
+
+router.patch('/users/:id', auth, updateSingleUser); // Update user by ID (Protected Route)
 
 router.get('/user/:id', auth,getUser); // Get single user by ID (Protected Route)
 
